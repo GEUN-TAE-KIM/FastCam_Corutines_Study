8b75ec2195f12b59a76af439f99c157c77912e2b
GEUN-TAE-KIM <rmsxo5678@naver.com>
2022-10-18 02:08:51 +0900

    join

---
+++ app/src/main/java/org/cream/corutines_practice_red/1장/스코프빌더/join.kt
@@ -0,0 +1,64 @@
+package org.cream.corutines_practice_red
+
+import kotlinx.coroutines.*
+
+/*
+Note
+ join = suspension이 되어 첫번째 런치블록이 끝날때까지 기다림
+ */
+suspend fun oneTwoThree() = coroutineScope {
+
+    val job = launch {
+        println("launch1: ${Thread.currentThread().name}")
+        delay(1000L)
+        println("4")
+    }
+    job.join()
+
+    launch {
+        println("launch2: ${Thread.currentThread().name}")
+        println("1")
+    }
+
+    launch {
+        println("launch3: ${Thread.currentThread().name}")
+        println("2")
+    }
+    println("3")
+
+}
+
+fun main12() = runBlocking {
+    oneTwoThree()
+    println("runBlocking: ${Thread.currentThread().name}")
+    println("5")
+}
+
+/*
+Note
+ repeat() = (숫자)를 반복한다.
+ */
+suspend fun oneTwoFive() = coroutineScope {
+
+    val job = launch {
+        println("launch1: ${Thread.currentThread().name}")
+        delay(1000L)
+        println("4")
+    }
+    job.join()
+
+    repeat(100) {
+        launch {
+            println("launch3: ${Thread.currentThread().name}")
+            println("2")
+        }
+    }
+
+}
+
+fun main() = runBlocking {
+    oneTwoFive()
+    println("runBlocking: ${Thread.currentThread().name}")
+    println("5")
+}
+
